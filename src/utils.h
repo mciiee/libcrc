4c96c527a051415eae8e9835a2313b01f36d9e70
mciiee <mrchingiz91@gmail.com>
2025-11-19 17:51:00 +0500

    utils: added string utility functions

--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,13 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#ifndef STRN_EQUALS
+#define STRN_EQUALS(left, right, n) strncmp(left, right, n) == 0
+#endif
+
+#ifndef STR_EQUALS
+#define STR_EQUALS(left, right) strcmp(left, right) == 0
+#endif
 
 char *strtok_rs(char * restrict str, const char *restrict delim, char **restrict saveptr);
 
