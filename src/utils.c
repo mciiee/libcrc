827e9933bc2bd49ebb20ae9778751348a8dc8a08
mciiee <mrchingiz91@gmail.com>
2025-11-20 13:07:30 +0500

    utils: fixed conditions in strtok_rs

--- src/utils.c
+++ src/utils.c
@@ -1,23 +1,25 @@
-#ifndef _POSIX_C_SOURCE
-#define _POSIX_C_SOURCE 200112L
 #include <string.h>
-#endif
 
 #include "utils.h"
 
-char *strtok_rs(char * restrict str, const char *restrict delim, char **restrict saveptr) {
-     #if !(defined (WIN32)) && !(defined (__WIN32))
-    return strtok_r(str, delim, saveptr);
-    #else 
-    // TODO: Finish this implementation for Windows
-    if(saveptr == nullptr) {
-        size_t i = 0;
-        for (; str[i] != '\0' && strcmp(str + i, delim) != 0 ; i++) {
-            
-        }   
+char *strtok_rs(char *str, const char *delim, char **saveptr) {
+    if (!str && !(str = *saveptr)) { 
+        return nullptr;
     }
-    else {
-
+    str += strspn(str, delim);
+    if (!str[0]) {
+        *saveptr = nullptr;
+        return nullptr;
     }
-    #endif
+    *saveptr = str + strcspn(str, delim);
+    if (**saveptr) {
+        **saveptr = 0;
+        (*saveptr)++;
+    }
+    else { 
+        *saveptr = nullptr;
+    }
+    return str;
 }
+
+
